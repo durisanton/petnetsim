afac038f8bb736b97da75121fa11a87c4a6b87b0
Anton Duris <toniid33@gmail.com>
2024-03-12 15:27:09 +0100

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='petnetsim',
+    version='1.0.0',
+    packages=['editor', 'petnetsim'],
+    url='',
+    license='MIT',
+    author='',
+    author_email='',
+    description=''
+)
